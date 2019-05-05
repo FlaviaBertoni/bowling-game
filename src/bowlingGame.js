50cc98fd83754a791d23ab1590e7941656835af4
FlaviaBertoni <flavia.boava.bertoni@gmail.com>
2019-05-05 14:23:56 -0300

    Replace const name

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -2,19 +2,19 @@ const createTotalPointsFrames = (rolls, frames = [], index = 0) => {
     if(frames.length === 10) return frames;
 
     const currentRoll = rolls[index];
-    const currentRollAndNextRollSum = currentRoll + rolls[index + 1];
+    const currentPlusNextRoll = currentRoll + rolls[index + 1];
 
     const isStrike = currentRoll === 10;
-    const isSpare = currentRollAndNextRollSum === 10;
+    const isSpare = currentPlusNextRoll === 10;
 
     if(isStrike || isSpare) {
-        const points = currentRollAndNextRollSum + rolls[index + 2];
+        const points = currentPlusNextRoll + rolls[index + 2];
         const nextIndex = isStrike ? index + 1 : index + 2;
 
         return createTotalPointsFrames(rolls, [...frames, points] , nextIndex);
     }
 
-    return createTotalPointsFrames(rolls, [...frames, currentRollAndNextRollSum] , index + 2);
+    return createTotalPointsFrames(rolls, [...frames, currentPlusNextRoll] , index + 2);
 };
 
 const score = (rolls = []) => {
